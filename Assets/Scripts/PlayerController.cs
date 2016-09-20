931f42a0c4ae2ab18a86b70ed329a376f7501cb5
CKiilu <cndeti@gmail.com>
2016-09-20 22:09:18 +0300

    Player jumping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb2D;
+    public float speed;
+    public Text vectorText;
+    private bool grounded;
+    public int jumpHeight;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        grounded = true;
+    }
+
+    void FixedUpdate()
+    {
+        move(Input.GetAxis("Horizontal"));
+        if (grounded && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            jump();
+        }
+
+    }
+    void move(float moveHorizontal)
+    {
+        Vector2 movement = new Vector2(moveHorizontal, 0f);
+        vectorText.text = "" + (movement * speed);
+        rb2D.AddForce(movement * speed);
+    }
+
+    void jump()
+    {
+        rb2D.AddForce(Vector3.up * jumpHeight);
+        grounded = false;
+    }
+}
