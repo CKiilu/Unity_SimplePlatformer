3e721845a0ba7f62a68165d40f86aaf34e29044f
CKiilu <cndeti@gmail.com>
2016-09-21 19:32:57 +0300

    Added change of scenes

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnemyController : MonoBehaviour {
+
+    
+    void OnFixedUpdate()
+    {
+        transform.position = new Vector3(50, 0, 0) * Time.deltaTime;
+    }
+	
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Dead");
+        }
+    }
+}
---
+++ Assets/Scripts/RestartScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("Scene1");
+        }
+    }
+}
