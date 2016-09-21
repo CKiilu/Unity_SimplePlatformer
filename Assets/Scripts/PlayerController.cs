efef949f68d041f6314f2f73ee44319ae3dd2978
CKiilu <cndeti@gmail.com>
2016-09-21 17:27:17 +0300

    rm multijump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,37 +5,77 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody2D rb2D;
+    private Rigidbody2D player;
     public float speed;
     public Text vectorText;
-    private bool grounded;
+    private bool grounded = true, climbing = false;
     public int jumpHeight;
 
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
-        grounded = true;
+        player = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        move(Input.GetAxis("Horizontal"));
-        if (grounded && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)))
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        move(moveHorizontal, 0);
+        if (grounded && moveVertical > 0 && !climbing)
         {
             jump();
         }
+        vectorText.text = climbing.ToString();
+
+        if (player.velocity.y == 0 && !grounded)
+        {
+            setGrounded(true);
+        }
+
+        if (climbing)
+        {
+            player.transform.Translate(new Vector3(0, (moveVertical > 0)? 1 : -1, 0) * Time.deltaTime);
+        }
 
     }
-    void move(float moveHorizontal)
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Vector2 movement = new Vector2(moveHorizontal, 0f);
-        vectorText.text = "" + (movement * speed);
-        rb2D.AddForce(movement * speed);
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            setClimbingBools(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            setClimbingBools(false);
+        }
+    }
+
+
+    void move(float moveHorizontal, float moveVertical)
+    {
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        player.AddForce(movement * speed);
     }
 
     void jump()
     {
-        rb2D.AddForce(Vector3.up * jumpHeight);
-        grounded = false;
+        player.AddForce(Vector3.up * jumpHeight);
+        setGrounded(false);
+    }
+
+    void setGrounded(bool val)
+    {
+        grounded = val;
+    }
+
+    void setClimbingBools(bool val)
+    {
+        climbing = val;
+        player.gravityScale = (val) ? 1 : 0;
     }
 }
